023716e702a4d0ee0d834d9433201e7c35e75116
Maxim Grachev <maxoowww@gmail.com>
2024-02-18 01:52:20 +0300

    Добавил светлую и темную тему

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,8 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+      <body className={cn(
+          font.className,
+          "bg-white dark:bg-[#313338]"
+        )}>
+            <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} storageKey="Let's talk theme">
+              {children}
+            </ThemeProvider>
+         </body>
       </html>
     </ClerkProvider>
   );
