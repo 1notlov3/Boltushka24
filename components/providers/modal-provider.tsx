774cf4e991a4bd2cc9806df9b794d4fe8a9b7013
Maxim Grachev <maxoowww@gmail.com>
2024-03-09 23:23:43 +0300

    Добавил настройки для сервера

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
   useEffect(()=>{
@@ -13,6 +14,7 @@ export const ModalProvider = () => {
   return (<>
     <CreateServerModal/>
     <InviteModal/>
+    <EditServerModal />
   </>
   )
 }
