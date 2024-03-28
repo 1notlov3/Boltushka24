d57766201a7c48d01a0a2d26c915828e34fa2b78
Maxim Grachev <maxoowww@gmail.com>
2024-03-28 18:51:43 +0300

    Сделал настройку ролей участников сервера

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { MembersModal } from "@/components/modals/members-modal";
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
   useEffect(()=>{
@@ -15,6 +16,7 @@ export const ModalProvider = () => {
     <CreateServerModal/>
     <InviteModal/>
     <EditServerModal />
+    <MembersModal/>
   </>
   )
 }
