108f725959d2c51b562bb21c01653fbdb2f26709
Maxim Grachev <maxoowww@gmail.com>
2024-04-04 17:03:37 +0300

    Добавил адаптацию интерфнйса для мобильных устройств

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,44 +1,51 @@
-import { ChatHeader } from "@/components/chat/chat-header";
-import { redirectToSignIn, RedirectToSignIn } from "@clerk/nextjs";
-import { currentProfile } from "@/lib/current-profile";
-import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-interface ChannelIdPageProps{
+
+import { currentProfile } from "@/lib/current-profile";
+import { ChatHeader } from "@/components/chat/chat-header";
+import { db } from "@/lib/db";
+
+interface ChannelIdPageProps {
   params: {
     serverId: string;
     channelId: string;
   }
 }
+
 const ChannelIdPage = async ({
   params
-}:ChannelIdPageProps) => {
+}: ChannelIdPageProps) => {
   const profile = await currentProfile();
-  if (!profile){
+
+  if (!profile) {
     return redirectToSignIn();
   }
+
   const channel = await db.channel.findUnique({
-    where:{
+    where: {
       id: params.channelId,
     },
   });
-  const member = await db.channel.findFirst({
-    where:{
-      serverId:params.serverId,
-      profileId:profile.id
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
     }
   });
-  if (!channel || !member){
-    redirect('/');
+
+  if (!channel || !member) {
+    redirect("/");
   }
-  return(
-    <div>
+  return ( 
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
       <ChatHeader
-      name={channel.name}
-      serverId={channel.serverId}
-      type = 'channel'
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
       />
     </div>
-  );
+   );
 }
 
 export default ChannelIdPage;
