bc23dcc55953f92cbe37e9e45f111da9ac63f4f5
1notlov3 <maxoowww@gmail.com>
2024-04-04 11:27:57 +0300

    Добавил редактирование каналов

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -16,7 +16,7 @@ export const NavigationAction = () => {
         <button onClick={() => onOpen("createServer")}
           className="group flex items-center"
         >
-          <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-white dark:bg-neutral-700 group-hover:bg-emerald-500">
+          <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-white dark:bg-neutral-700 group-hover:bg-blue-500 dark:group-hover:bg-emerald-500">
             <Plus
               className="group-hover:text-white transition text-blue-500 dark:text-emerald-500"
               size={25}
