26bf75b0856cdad3da527f35088d29cdf238c5b6
root <root@eu-node-1.play2go.cloud>
2026-01-25 19:22:14 +0000

    Fix setup route and allow socket.io endpoint through Clerk middleware

---
+++ app/setup/page.tsx
@@ -0,0 +1,26 @@
+import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/initial-modal";
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+
+const SetupPage = async () => {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <InitialModal />;
+};
+
+export default SetupPage;
